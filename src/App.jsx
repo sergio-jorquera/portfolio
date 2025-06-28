aa0a18cfc3d05c54ee2f0b7b7cb91037dbd3eae6
sergio-jorquera <sjorkera@gmail.com>
2025-06-28 21:20:15 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-purple-600">¡Tailwind v4 funcionando!</h1>
+    </>
+  )
+}
+
+export default App
